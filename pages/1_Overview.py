7707f23094133ea09c67e541e71cfa4d34fdbfee
ohm <160294328+jackeyoo@users.noreply.github.com>
2026-03-18 11:49:10 +0700

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -9,8 +9,8 @@ def render_sidebar():
         st.markdown("## ⚡ Nature Biotech")
 
         st.page_link("streamlit_app.py", label="📊 Overview")
-        st.page_link("pages/1_Equipment.py", label="⚙️ Equipment")
-        st.page_link("pages/2_Solar.py", label="☀️ Solar")
+        st.page_link("pages/2_Equipment.py", label="⚙️ Equipment")
+        st.page_link("pages/3_Solar.py", label="☀️ Solar")
 
         st.divider()
 
