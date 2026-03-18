852aa5453d787f49e85c9ab42e357598247046c4
ohm <160294328+jackeyoo@users.noreply.github.com>
2026-03-18 15:01:52 +0700

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -1,10 +1,10 @@
+st.set_page_config(page_title="Overview", page_icon="📊", layout="wide")
+apply_style()
 import streamlit as st
 import plotly.express as px
 from utils import safe_load, find_col, to_num
 from style import apply_style, plot_theme
 st.sidebar.markdown("## ⚡ Nature Biotech")
-st.set_page_config(page_title="Overview", page_icon="📊", layout="wide")
-apply_style()
 
 
 if st.sidebar.button("🔄 Refresh Data"):
