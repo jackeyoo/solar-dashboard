bda548f0223968696284effeebf0698a5fc345a3
ohm <160294328+jackeyoo@users.noreply.github.com>
2026-03-30 17:32:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
-import streamlit as st
+# import streamlit as st
 
-st.title("Solar Dashboard")
+# st.title("Solar Dashboard")
+
+# st.image("assets/NB256-o2zjy16wije0o2airfsyfibi0gvu6yx706i1i9tu80.jpg", use_container_width=True)
+# st.markdown("""
+# <style>
+# .stApp {
+#     background-image: url("assets/assets/NB256-o2zjy16wije0o2airfsyfibi0gvu6yx706i1i9tu80.jpg");
+#     background-size: cover;
+#     background-position: center;
+#     background-repeat: no-repeat;
+# }
+# </style>
+# """, unsafe_allow_html=True)
+
+# st.title("⚡ Solar Dashboard")
+
+import streamlit as st
+from style import apply_style
+
+st.set_page_config(page_title="Energy Dashboard", layout="wide")
+apply_style()
+st.empty()
