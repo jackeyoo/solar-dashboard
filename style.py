3db1a35664a127a384397c2474a21fb419e923b0
ohm <160294328+jackeyoo@users.noreply.github.com>
2026-03-30 17:32:46 +0700

    Update style.py

--- style.py
+++ style.py
@@ -5,7 +5,7 @@ def apply_style():
     <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <style>
-    #MainMenu {visibility: hidden;}
+    MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
 
     [data-testid="stSidebarNav"] ul li:first-child {
